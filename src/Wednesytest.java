0cd9daaf1c3ab7b15b1209a0e9fb7930dc9fef21
user <user@cyber>
2017-03-15 16:32:18 +0100

    1860

---
+++ src/Wednesytest.java
@@ -0,0 +1,69 @@
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Wednesytest {
+    static String book_name,book_id,book_author;
+
+    public static String getBook_name() {
+        return book_name;
+    }
+
+    public static void setBook_name(String book_name) {
+        Wednesytest.book_name = book_name;
+    }
+
+    public static String getBook_id() {
+        return book_id;
+    }
+
+    public static void setBook_id(String book_id) {
+        Wednesytest.book_id = book_id;
+    }
+
+    public static String getBook_author() {
+        return book_author;
+    }
+
+    public static void setBook_author(String book_author) {
+        Wednesytest.book_author = book_author;
+    }
+
+    public static String getYear() {
+        return year;
+    }
+
+    public static void setYear(String year) {
+        Wednesytest.year = year;
+    }
+
+    public static String getISSN() {
+        return ISSN;
+    }
+
+    public static void setISSN(String ISSN) {
+        Wednesytest.ISSN = ISSN;
+    }
+    static String year,ISSN;
+    
+    
+}
+ // public static void writefile(String content)throws IOException
+
+  //public static void (String InputBOOKname, String InputBooKID, String InputBookKauthor, String InputYear, String InputISSN)
+
+//try{
+    //  Connection myCon;
+      //Statement myST;
+
+//}
